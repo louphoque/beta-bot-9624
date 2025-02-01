cee3de4ee3fc6559c0a508d2bbb72743243536fe
levalex01 <levalex0107@gmail.com>
2025-01-31 22:57:25 -0500

    update

--- cam project/src/main/cpp/Robot.cpp
+++ cam project/src/main/cpp/Robot.cpp
@@ -23,42 +23,41 @@ class Robot : public frc::TimedRobot {
     visionThread.detach();
   }
 
- private:
-  static void VisionThread() {
-    // Create an HTTP camera. The address will need to be modified to have the
-    // correct team number. The exact path will depend on the source.
-    cs::HttpCamera camera{"My Camera", "http://10.x.y.11/video/stream.mjpg"};
-    // Start capturing images
-    frc::CameraServer::StartAutomaticCapture(camera);
-    // Set the resolution
-    camera.SetResolution(640, 480);
+  private:
+    static void VisionThread() {
+      // Get the USB camera from CameraServer
+      cs::UsbCamera camera = frc::CameraServer::StartAutomaticCapture();
+      // Set the resolution
+      camera.SetResolution(640, 480);
 
-    // Get a CvSink. This will capture Mats from the Camera
-    cs::CvSink cvSink = frc::CameraServer::GetVideo();
-    // Setup a CvSource. This will send images back to the Dashboard
-    cs::CvSource outputStream =
-        frc::CameraServer::PutVideo("Rectangle", 640, 480);
+      // Get a CvSink. This will capture Mats from the Camera
+      cs::CvSink cvSink = frc::CameraServer::GetVideo();
+      // Setup a CvSource. This will send images back to the Dashboard
+      cs::CvSource outputStream =
+          frc::CameraServer::PutVideo("Rectangle", 640, 480);
 
-    // Mats are very memory expensive. Lets reuse this Mat.
-    cv::Mat mat;
+      // Mats are very memory expensive. Lets reuse this Mat.
+      cv::Mat mat;
 
-    while (true) {
-      // Tell the CvSink to grab a frame from the camera and put it in the
-      // source mat. If there is an error notify the output.
-      if (cvSink.GrabFrame(mat) == 0) {
-        // Send the output the error.
-        outputStream.NotifyError(cvSink.GetError());
-        // skip the rest of the current iteration
-        continue;
+      while (true) {
+        // Tell the CvSink to grab a frame from the camera and
+        // put it
+        // in the source mat.  If there is an error notify the
+        // output.
+        if (cvSink.GrabFrame(mat) == 0) {
+          // Send the output the error.
+          outputStream.NotifyError(cvSink.GetError());
+          // skip the rest of the current iteration
+          continue;
+        }
+        // Put a rectangle on the image
+        rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
+                  cv::Scalar(255, 255, 255), 5);
+        // Give the output stream a new image to display
+        outputStream.PutFrame(mat);
       }
-      // Put a rectangle on the image
-      rectangle(mat, cv::Point(100, 100), cv::Point(400, 400),
-                cv::Scalar(255, 255, 255), 5);
-      // Give the output stream a new image to display
-      outputStream.PutFrame(mat);
     }
-  }
-};
+  };
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
